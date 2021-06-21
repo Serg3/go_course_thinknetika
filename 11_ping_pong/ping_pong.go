ebf70ffc5995932738f689a9d3f19177efbde578
Serhii Kovalov <serg3@Serhiis-MacBook-Pro.local>
2021-06-22 01:22:00 +0200

    simple trying

--- 11_ping_pong/ping_pong.go
+++ 11_ping_pong/ping_pong.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func game(wg *sync.WaitGroup, p string) {
+	fmt.Println("Player", p)
+	wg.Done()
+}
+
+func main() {
+	p1, p2 := "Biba", "Boba"
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go game(&wg, p1)
+	go game(&wg, p2)
+	wg.Wait()
 }
