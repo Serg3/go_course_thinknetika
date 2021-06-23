463016eb94bb4806a33a3a1cf8611d5643467b22
kovalov <kovalov-ext@fidor.com>
2021-06-23 10:20:05 +0200

    init 12

---
+++ 12_search_engine/pkg/storage/file.go
@@ -0,0 +1,63 @@
+package storage
+
+import (
+	"bufio"
+	"encoding/json"
+	"go_course_thinknetika/12_search_engine/pkg/crawler"
+	"io"
+)
+
+type Filestore struct{}
+
+func New() *Filestore {
+	return &Filestore{}
+}
+
+func (f *Filestore) Load(src io.Reader) ([]crawler.Document, error) {
+	b, err := read(src)
+	if err != nil {
+		return nil, err
+	}
+
+	crwDocs := make([]crawler.Document, 0)
+
+	err = json.Unmarshal(b, &crwDocs)
+	if err != nil {
+		return nil, err
+	}
+
+	return crwDocs, nil
+}
+
+func (f *Filestore) Save(src io.Writer, crwDocs []crawler.Document) error {
+	b, err := json.Marshal(crwDocs)
+	if err != nil {
+		return err
+	}
+
+	err = write(src, b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func read(r io.Reader) ([]byte, error) {
+	s := bufio.NewScanner(r)
+	var b []byte
+
+	for s.Scan() {
+		b = append(b, []byte(s.Text()+"\n")...)
+	}
+	if err := s.Err(); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func write(w io.Writer, b []byte) error {
+	_, err := w.Write(b)
+	return err
+}
