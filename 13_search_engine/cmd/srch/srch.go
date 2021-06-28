f1cbbb60a4ddf1e6df2bb136def99ecbd55d2dcd
Serhii Kovalov <serg3@Serhiis-MacBook-Pro.local>
2021-06-28 22:20:23 +0200

    refactoring according to comments

--- 13_search_engine/cmd/srch/srch.go
+++ 13_search_engine/cmd/srch/srch.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"go_course_thinknetika/13_search_engine/pkg/crawler"
+	"go_course_thinknetika/13_search_engine/pkg/crawler/spider"
 	"go_course_thinknetika/13_search_engine/pkg/netsrv"
 	"log"
+	"sync"
 )
 
 func main() {
-	err := netsrv.ListenAndSearch([]string{"https://golang.org", "https://go.dev"}, 2)
-	// errors handler from net service
+	var docs []crawler.Document
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		docs = scan([]string{"https://golang.org", "https://go.dev"}, 2)
+		wg.Done()
+	}()
+
+	listener, err := netsrv.ListenAndSearch("tcp", ":8000")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(docs) == 0 {
+			conn.Write([]byte("Buffer is empty. A new scan is in progress...\n"))
+			wg.Wait()
+		}
+		go netsrv.Searcher(conn, docs)
+	}
+}
+
+func scan(urls []string, depth int) (docs []crawler.Document) {
+	scn := spider.New()
+	for _, url := range urls {
+		res, err := scn.Scan(url, depth)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		docs = append(docs, res...)
+	}
+	return docs
 }
--- 13_search_engine/pkg/netsrv/netsrv.go
+++ 13_search_engine/pkg/netsrv/netsrv.go
@@ -3,56 +3,23 @@ package netsrv
 import (
 	"bufio"
 	"go_course_thinknetika/13_search_engine/pkg/crawler"
-	"go_course_thinknetika/13_search_engine/pkg/crawler/spider"
-	"log"
 	"net"
 	"strings"
 )
 
-type SearchParams struct {
-	urls  []string
-	depth int
-}
-
-// ListenAndSearch accepts two parameters for URLs ([]string) and depth (int),
-// performs a new scan of provided sites with specific depth
-// and listen "tcp" network addresses on port ":8000".
+// ListenAndSearch accepts two parameters for network (string) and address (string).
 // Returns error if something went wrong with network connection.
-func ListenAndSearch(urls []string, depth int) error {
-	sp := SearchParams{}
-	sp.urls = urls
-	sp.depth = depth
-
-	l, err := net.Listen("tcp", ":8000")
+func ListenAndSearch(network, address string) (net.Listener, error) {
+	l, err := net.Listen(network, address)
 	if err != nil {
-		return err
+		return nil, err
 	}
-
-	err = handler(l, sp)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return l, nil
 }
 
-// Handler performs handling of incoming connections
-// from the listener and parameters SearchParams
-// given to the function
-// and provides a search result from scanned sites
-// by incoming word from the reader.
-func handler(listener net.Listener, sp SearchParams) error {
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			return err
-		}
-		go searcher(conn, sp)
-	}
-}
-
-func searcher(conn net.Conn, sp SearchParams) {
-	var docs []crawler.Document
+// Function Searcher() performs handling of incoming connections
+// and makes a search by received word through provided documents.
+func Searcher(conn net.Conn, docs []crawler.Document) {
 	defer conn.Close()
 
 	r := bufio.NewReader(conn)
@@ -64,10 +31,6 @@ func searcher(conn net.Conn, sp SearchParams) {
 		}
 
 		if len(msg) > 0 {
-			if len(docs) == 0 {
-				conn.Write([]byte("Buffer is empty. Performing a new scan...\n"))
-				docs = scan(sp)
-			}
 			conn.Write([]byte("Search result:\n"))
 			for _, doc := range docs {
 				if strings.Contains(strings.ToLower(doc.Title), strings.ToLower(string(msg))) {
@@ -81,20 +44,4 @@ func searcher(conn net.Conn, sp SearchParams) {
 			return
 		}
 	}
-}
-
-// Function scan() uses package 'crawler'
-// to search through Go sites by word
-// and returs []crawler.Document result
-func scan(sp SearchParams) (docs []crawler.Document) {
-	scn := spider.New()
-	for _, url := range sp.urls {
-		res, err := scn.Scan(url, sp.depth)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		docs = append(docs, res...)
-	}
-	return docs
 }
