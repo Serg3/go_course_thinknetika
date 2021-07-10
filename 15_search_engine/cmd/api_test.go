b5dd7f390d1a5e18deeba7512e4f6d070482587d
Serhii Kovalov <serg3@Serhiis-MacBook-Pro.local>
2021-07-10 15:10:51 +0200

    /docs test

--- 15_search_engine/cmd/api_test.go
+++ 15_search_engine/cmd/api_test.go
@@ -22,28 +22,14 @@ func TestMain(m *testing.M) {
 }
 
 func TestAPI_docs(t *testing.T) {
-	type fields struct {
-		router *mux.Router
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/docs", nil)
+	rr := httptest.NewRecorder()
+	api.router.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("wrong code: got %d, want %d", rr.Code, http.StatusOK)
 	}
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			api := &API{
-				router: tt.fields.router,
-			}
-			api.docs(tt.args.w, tt.args.r)
-		})
-	}
+	t.Log("Response: ", rr.Body)
 }
 
 func TestAPI_newDoc(t *testing.T) {
